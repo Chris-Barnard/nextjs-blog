7041464fd49e53f4510a5ff71f7f105096fdd439
Chris Barnard <barnard.chris@gmail.com>
2023-10-11 16:27:30 -0600

    Adds in a responsive navbar for better viewing on mobile

--- components/react-markdown.js
+++ components/react-markdown.js
@@ -15,6 +15,16 @@ export default function Markdown({ content, imageSizes }) {
               // if we couldn't determine the image's dimensions, resort to a classic img element
               return <img {...props} />
             }
+          },
+          a: (props) => {
+            // search for external links in our blog post and have them open in a new tab
+            const url = props.href;
+            const external = url.startsWith("https://") || url.startsWith("http://")
+            if (external) {
+                return <a {...props} target="_blank" />
+            } else {
+                return <a {...props} />
+            }
           }
         }}
       >
